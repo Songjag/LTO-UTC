2ede7d83293c39426a5e2ee3c4c86b7ab760d531
Songjag <elysiabot301127@gmail.com>
2025-10-03 06:29:15 +0700

    c.

---
+++ c/2.1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    long long a,b;
+    long long thuong,du;
+    scanf("%lld %lld",&a,&b);
+    thuong=a/b;
+    du=a%b;
+    printf("%lld %lld\n",thuong,du);
+    return 0;
+}
+//url:https://laptrinhonline.club/problem/0007dayc
--- python/2x/2.1.py
+++ python/2x/2.1.py
@@ -1,2 +1,3 @@
 a,b=map(int,input().split())
 print(f'{int(a/b)} {int(a%b)}')
+#url:https://laptrinhonline.club/problem/0007dayc
